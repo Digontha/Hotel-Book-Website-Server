b8d0f6b78ce1faa14326ec848167f782f655dd01
Digontha <digonthodj@gmail.com>
2023-11-05 20:20:11 +0600

    create single data api

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get("/",(req,res)=>{
 })
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.jp082z4.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -62,6 +62,13 @@ async function run() {
         const cursor =  roomCollection.find().sort(sortObj);
         const result = await cursor.toArray();
         res.send(result);
+    });
+
+    app.get("/details/:id",async(req,res) => {
+       const id = req.params.id
+       const query = {_id : new ObjectId(id)};
+       const result= await roomCollection.findOne(query);
+       res.send(result);
     });
 
     // Send a ping to confirm a successful connection
